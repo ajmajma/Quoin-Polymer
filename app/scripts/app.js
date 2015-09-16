95971f4c6f1353b80c7d98d31f9ea4814d151d34
Moser, Alex <Alex.Moser@contractor.cengage.com>
2015-09-16 15:07:10 -0400

    content and layout work

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -22,7 +22,7 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   // Listen for template bound event to know when bindings
   // have resolved and content has been stamped to the page
   app.addEventListener('dom-change', function() {
-    console.log('App');
+    console.log('App is live');
   });
 
   // See https://github.com/Polymer/polymer/issues/1381
